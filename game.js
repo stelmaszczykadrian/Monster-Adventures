581d77972e12f5b2d754cf8b2e6f2987fdf2369d
reizergrzegorz <reizergrzegorz@gmail.com>
2022-10-18 09:55:31 +0200

    add loosing parameters

--- game.js
+++ game.js
@@ -5,3 +5,10 @@ function initGame() {
     // Your game can start here, but define separate functions, don't write everything in here :)
 
 }
+
+var checkIfLoose = setInterval(function(){
+    var monsterTop = (window.getComputedStyle(monster).getPropertyValue('top'));
+    var  obstacle = (window.getComputedStyle(obstacle).getPropertyValue('top'));
+
+
+},10);
